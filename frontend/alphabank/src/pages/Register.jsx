0cdcf643e21531b00065388084a9283b9d7729ee
Victor <victorhscampos@gmail.com>
2022-06-14 18:53:02 -0300

    up5

---
+++ frontend/alphabank/src/pages/Register.jsx
@@ -0,0 +1,17 @@
+import Main from "./components/Main/Main";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { GlobalStyle } from "./style/global";
+
+function Register() {
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+      <registerMain />
+      <Footer />
+    </>
+  );
+}
+
+export default Register;
